6bd98c142f0e5758756510e997b22d97b168e48d
JorgeVargas <javargas216@unicauca.edu.co>
2021-09-30 15:59:39 -0500

    Arreglo del btn.secondary y funcion para el input tel

--- js/index.js
+++ js/index.js
@@ -1,11 +1,28 @@
 var inpCelular = document.getElementById("inpCelular");
+var a = 0;
 
 inpCelular.addEventListener("keyup",function(){
     
-    var n = inp.value;
-    
-    if(n==3){
-        
+    var tel = inpCelular.value;   
+
+    if(tel.length > 0 && tel.length < 3 ){
+        a = 0;
     }
+
+    if(tel.length > 3 && tel.length < 7 ){
+        a = 1;
+    }
+
+    if(tel.length==3 && a == 0){
+        tel = tel+"-";
+        inpCelular.value = tel;
+        a = 1;
+    }
+
+    if(tel.length==7 && a == 1){
+        tel = tel+"-";
+        inpCelular.value = tel;
+        a = 2;
+    }   
 
 });
