f9e3a8bbe7e8e7335c0f650e54a117763796b7a0
JorgeVargas <javargas216@unicauca.edu.co>
2021-09-30 16:04:33 -0500

    Rename Flag JS

--- js/index.js
+++ js/index.js
@@ -1,28 +1,28 @@
 var inpCelular = document.getElementById("inpCelular");
-var a = 0;
+var flag = 0;
 
 inpCelular.addEventListener("keyup",function(){
     
     var tel = inpCelular.value;   
 
     if(tel.length > 0 && tel.length < 3 ){
-        a = 0;
+        flag = 0;
     }
 
     if(tel.length > 3 && tel.length < 7 ){
-        a = 1;
+        flag = 1;
     }
 
-    if(tel.length==3 && a == 0){
+    if(tel.length==3 && flag == 0){
         tel = tel+"-";
         inpCelular.value = tel;
-        a = 1;
+        flag = 1;
     }
 
-    if(tel.length==7 && a == 1){
+    if(tel.length==7 && flag == 1){
         tel = tel+"-";
         inpCelular.value = tel;
-        a = 2;
+        flag = 2;
     }   
 
 });
