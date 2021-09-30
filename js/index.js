eb8fb9b7246bff5c6d4be125d840f15bbbdc6ee5
JorgeVargas <javargas216@unicauca.edu.co>
2021-09-30 13:32:18 -0500

    Interfaz V1 Co-authored-by: JoseMBetancourt

---
+++ js/index.js
@@ -0,0 +1,11 @@
+var inpCelular = document.getElementById("inpCelular");
+
+inpCelular.addEventListener("keyup",function(){
+    
+    var n = inp.value;
+    
+    if(n==3){
+        
+    }
+
+});
