5c499601d2c63871df4d67a1e6992747966870ca
JoseMBetancourt <josebetancourt@unicauca.edu.co>
2021-10-14 18:19:59 -0500

    pop up card

--- index.php
+++ index.php
@@ -205,7 +205,7 @@ if ($mysqli->connect_error) {
 							</a>
 						</div>
 					</div>
-					
+
 				</div>
 
 
@@ -234,12 +234,12 @@ if ($mysqli->connect_error) {
 				<div class="bg-close" id="bg-close"></div>
 				<div class="card-content">
 					<div class="card-header">
-						<div class="logo"></div>
-						<h2>Membresía Gana tu Carro</h2>
+						<div class="logo me-1"></div>
+						<h2 class="me-1">Membresía Gana tu Carro</h2>
 						<i class="fas fa-2x fa-times" id="btnClose"></i>
 					</div>
-					<div class="card-info-transaction">
-						<div class="ticket">
+					<div class="card-info-transaction virtual-ticket">
+						<div class="ticket mb-1">
 							<svg min-width="175" min-height="117" width="100%" height="100%" viewBox="0 0 175 117" fill="none" xmlns="http://www.w3.org/2000/svg">
 								<path class="ticket-svg" d="M38.8889 29.25H136.111V87.75H38.8889V29.25ZM160.417 58.5C160.417 66.5773 166.946 73.125 175 73.125V102.375C175 110.452 168.471 117 160.417 117H14.5833C6.52908 117 0 110.452 0 102.375V73.125C8.05425 73.125 14.5833 66.5773 14.5833 58.5C14.5833 50.4227 8.05425 43.875 0 43.875V14.625C0 6.54773 6.52908 0 14.5833 0H160.417C168.471 0 175 6.54773 175 14.625V43.875C166.946 43.875 160.417 50.4227 160.417 58.5ZM145.833 26.8125C145.833 22.7739 142.569 19.5 138.542 19.5H36.4583C32.4312 19.5 29.1667 22.7739 29.1667 26.8125V90.1875C29.1667 94.2261 32.4312 97.5 36.4583 97.5H138.542C142.569 97.5 145.833 94.2261 145.833 90.1875V26.8125Z" fill="url(#silver)" />
 								<defs>
@@ -257,13 +257,26 @@ if ($mysqli->connect_error) {
 							</svg>
 							<h1><?php echo $numero_boleta; ?></h1>
 						</div>
+
+						<table>
+							<tr>
+								<th>Nombre</th>
+								<td><?php echo $comprador_nombre; ?></td>
+							</tr>
+							<tr>
+								<th>Cedula</th>
+								<td><?php echo $comprador_cedula; ?></td>
+							</tr>
+							<tr>
+								<th>Referencia de pago</th>
+								<td><?php echo $referencia_pago; ?></td>
+							</tr>
+						</table>
 					</div>
-					<div>
-						<p>Nombre: <?php echo $comprador_nombre; ?></p>
-						<p>Cedula: <?php echo $comprador_cedula; ?></p>
-						<p>Referencia de pago: <?php echo $referencia_pago; ?></p>
-					</div>
-					<div class="card-footer">
+
+
+
+					<div class="card-footer mt-1">
 						<button class="btn btn-primary" id="btnAceptar">Aceptar</button>
 					</div>
 				</div>
