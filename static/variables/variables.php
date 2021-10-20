d0bb2e19d3c8998cca3e7e378b80a8e2565c2fa8
Lina Virginia <linavm@unicauca.edu.co>
2021-10-20 12:28:38 -0500

    Co-authored-by: jfredyromero <jfredyromero@users.noreply.github.com>

--- static/variables/variables.php
+++ static/variables/variables.php
@@ -5,4 +5,8 @@ $accountId = "512321";
 $dominio = "https://www.ganatucarro.com";
 // $dominio = "localhost/rifa-web-page";
 $price = 80000;
+$fecha_sorteo = date_create("2021-12-24 00:00:00");
+echo $fecha_sorteo;
+$fecha_sorteo = date("Y-m-d H:i:s", mktime(0, 0, 0, 12, 24, 2021));
+echo $fecha_sorteo;
 ?>
