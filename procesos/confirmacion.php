dfba5108af3b383dc1bc8d6f19f5a5cfe36c5b7c
Lina Virginia <linavm@unicauca.edu.co>
2021-11-06 18:31:47 -0500

    Solucionado problema de hotmail en confirmacion y el ticketmodel

--- procesos/confirmacion.php
+++ procesos/confirmacion.php
@@ -80,7 +80,7 @@ if($method == "POST"){
                 $framesize = 3;
                 $link = $dominio."?id_transaccion=".urlencode($id_transaccion)."&comprador_nombre=".urlencode($comprador_nombre)."&comprador_cedula=".urlencode($comprador_cedula)."&numero_boleta=".urlencode($boleta);
                 QRcode ::png($link, $rutaQR, $level, $tamaño, $framesize);   
-                $imagenesHTML = $imagenesHTML.'<p style="color: #ffffff;font-size:22px"><strong>MEMBRESÍA
+                $imagenesHTML = $imagenesHTML.'<p style="color: #ffffff;font-size:22px"><strong>CUPO
                     # '.$boleta.'<br></strong><br></p> <img src="'.$dominio.'/media/codigosQR/'.$nombreArchivo.'" width="400px" ></img><br>';
             }
             mysqli_close($connection);
@@ -110,9 +110,7 @@ if($method == "POST"){
             $destinatario = $comprador_correo; 
             $asunto = "La compra de tu cupo ha sido exitosa!"; 
             $cuerpo = ' 
-            <!doctype html><html ⚡4email data-css-strict><head><meta charset="utf-8"><style
-            amp4email-boilerplate>body{visibility:hidden}</style><script async
-            src="https://cdn.ampproject.org/v0.js"></script><style amp-custom>.es-desk-hidden {	display:none;	float:left;	overflow:hidden;	width:0;	max-height:0;	line-height:0;}s {	text-decoration:line-through;}body {	width:100%;	font-family:"open sans", "helvetica neue", helvetica, arial, sans-serif;}table {	border-collapse:collapse;	border-spacing:0px;}table td, html, body, .es-wrapper {	padding:0;	Margin:0;}.es-content, .es-header, .es-footer {	table-layout:fixed;	width:100%;}p, hr {	Margin:0;}h1, h2, h3, h4, h5 {	Margin:0;	line-height:120%;	font-family:"open sans", "helvetica neue", helvetica, arial, sans-serif;}.es-left {	float:left;}.es-right {	float:right;}.es-p5 {	padding:5px;}.es-p5t {	padding-top:5px;}.es-p5b {	padding-bottom:5px;}.es-p5l {	padding-left:5px;}.es-p5r {	padding-right:5px;}.es-p10 {	padding:10px;}.es-p10t {	padding-top:10px;}.es-p10b {	padding-bottom:10px;}.es-p10l {	padding-left:10px;}.es-p10r {	padding-right:10px;}.es-p15 {	padding:15px;}.es-p15t {	padding-top:15px;}.es-p15b {	padding-bottom:15px;}.es-p15l {	padding-left:15px;}.es-p15r {	padding-right:15px;}.es-p20 {	padding:20px;}.es-p20t {	padding-top:20px;}.es-p20b {	padding-bottom:20px;}.es-p20l {	padding-left:20px;}.es-p20r {	padding-right:20px;}.es-p25 {	padding:25px;}.es-p25t {	padding-top:25px;}.es-p25b {	padding-bottom:25px;}.es-p25l {	padding-left:25px;}.es-p25r {	padding-right:25px;}.es-p30 {	padding:30px;}.es-p30t {	padding-top:30px;}.es-p30b {	padding-bottom:30px;}.es-p30l {	padding-left:30px;}.es-p30r {	padding-right:30px;}.es-p35 {	padding:35px;}.es-p35t {	padding-top:35px;}.es-p35b {	padding-bottom:35px;}.es-p35l {	padding-left:35px;}.es-p35r {	padding-right:35px;}.es-p40 {	padding:40px;}.es-p40t {	padding-top:40px;}.es-p40b {	padding-bottom:40px;}.es-p40l {	padding-left:40px;}.es-p40r {	padding-right:40px;}.es-menu td {	border:0;}a {	text-decoration:none;}p, ul li, ol li {	font-family:"open sans", "helvetica neue", helvetica, arial, sans-serif;	line-height:150%;}ul li, ol li {	Margin-bottom:15px;}.es-menu td a {	text-decoration:none;	display:block;	font-family:"open sans", "helvetica neue", helvetica, arial, sans-serif;}.es-menu amp-img, .es-button amp-img {	vertical-align:middle;}.es-wrapper {	width:100%;	height:100%;}.es-wrapper-color {	background-color:#F6F6F6;}.es-header {	background-color:#3D4C6B;}.es-header-body {	background-color:#3D4C6B;}.es-header-body p, .es-header-body ul li, .es-header-body ol li {	color:#B7BDC9;	font-size:16px;}.es-header-body a {	color:#B7BDC9;	font-size:16px;}.es-content-body {	background-color:#F6F6F6;}.es-content-body p, .es-content-body ul li, .es-content-body ol li {	color:#999999;	font-size:16px;}.es-content-body a {	color:#75B6C9;	font-size:16px;}.es-footer {	background-color:transparent;}.es-footer-body {	background-color:transparent;}.es-footer-body p, .es-footer-body ul li, .es-footer-body ol li {	color:#999999;	font-size:14px;}.es-footer-body a {	color:#999999;	font-size:14px;}.es-infoblock, .es-infoblock p, .es-infoblock ul li, .es-infoblock ol li {	line-height:120%;	font-size:12px;	color:#CCCCCC;}.es-infoblock a {	font-size:12px;	color:#CCCCCC;}h1 {	font-size:40px;	font-style:normal;	font-weight:bold;	color:#444444;}h2 {	font-size:28px;	font-style:normal;	font-weight:normal;	color:#444444;}h3 {	font-size:18px;	font-style:normal;	font-weight:normal;	color:#444444;}.es-header-body h1 a, .es-content-body h1 a, .es-footer-body h1 a {	font-size:40px;}.es-header-body h2 a, .es-content-body h2 a, .es-footer-body h2 a {	font-size:28px;}.es-header-body h3 a, .es-content-body h3 a, .es-footer-body h3 a {	font-size:18px;}a.es-button, button.es-button {	border-style:solid;	border-color:#75B6C9;	border-width:15px 25px 15px 25px;	display:inline-block;	background:#75B6C9;	border-radius:28px;	font-size:18px;	font-family:"open sans", "helvetica neue", helvetica, arial, sans-serif;	font-weight:normal;	font-style:normal;	line-height:120%;	color:#FFFFFF;	text-decoration:none;	width:auto;	text-align:center;}.es-button-border {	border-style:solid solid solid solid;	border-color:#75B6C9 #75B6C9 #75B6C9 #75B6C9;	background:#75B6C9;	border-width:1px 1px 1px 1px;	display:inline-block;	border-radius:28px;	width:auto;}@media only screen and (max-width:600px) {p, ul li, ol li, a { line-height:150% } h1, h2, h3, h1 a, h2 a, h3 a { line-height:120% } h1 { font-size:30px; text-align:center } h2 { font-size:26px; text-align:center } h3 { font-size:20px; text-align:center } .es-header-body h1 a, .es-content-body h1 a, .es-footer-body h1 a { font-size:30px } .es-header-body h2 a, .es-content-body h2 a, .es-footer-body h2 a { font-size:26px } .es-header-body h3 a, .es-content-body h3 a, .es-footer-body h3 a { font-size:20px } .es-menu td a { font-size:16px } .es-header-body p, .es-header-body ul li, .es-header-body ol li, .es-header-body a { font-size:16px } .es-content-body p, .es-content-body ul li, .es-content-body ol li, .es-content-body a { font-size:16px } .es-footer-body p, .es-footer-body ul li, .es-footer-body ol li, .es-footer-body a { font-size:16px } .es-infoblock p, .es-infoblock ul li, .es-infoblock ol li, .es-infoblock a { font-size:12px } *[class="gmail-fix"] { display:none } .es-m-txt-c, .es-m-txt-c h1, .es-m-txt-c h2, .es-m-txt-c h3 { text-align:center } .es-m-txt-r, .es-m-txt-r h1, .es-m-txt-r h2, .es-m-txt-r h3 { text-align:right } .es-m-txt-l, .es-m-txt-l h1, .es-m-txt-l h2, .es-m-txt-l h3 { text-align:left } .es-m-txt-r amp-img { float:right } .es-m-txt-c amp-img { margin:0 auto } .es-m-txt-l amp-img { float:left } .es-button-border { display:block } a.es-button, button.es-button { font-size:20px; display:block; border-width:15px 25px 15px 25px } .es-btn-fw { border-width:10px 0px; text-align:center } .es-adaptive table, .es-btn-fw, .es-btn-fw-brdr, .es-left, .es-right { width:100% } .es-content table, .es-header table, .es-footer table, .es-content, .es-footer, .es-header { width:100%; max-width:600px } .es-adapt-td { display:block; width:100% } .adapt-img { width:100%; height:auto } td.es-m-p0 { padding:0px } td.es-m-p0r { padding-right:0px } td.es-m-p0l { padding-left:0px } td.es-m-p0t { padding-top:0px } td.es-m-p0b { padding-bottom:0 } td.es-m-p20b { padding-bottom:20px } .es-mobile-hidden, .es-hidden { display:none } tr.es-desk-hidden, td.es-desk-hidden, table.es-desk-hidden { width:auto; overflow:visible; float:none; max-height:inherit; line-height:inherit } tr.es-desk-hidden { display:table-row } table.es-desk-hidden { display:table } td.es-desk-menu-hidden { display:table-cell } .es-menu td { width:1% } table.es-table-not-adapt, .esd-block-html table { width:auto } table.es-social { display:inline-block } table.es-social td { display:inline-block } }</style></head>
+            <!doctype html><html><head><meta charset="utf-8"></head>
     <body><div class="es-wrapper-color"> <!--[if gte mso 9]><v:background xmlns:v="urn:schemas-microsoft-com:vml" fill="t"> <v:fill type="tile" color="#f6f6f6"></v:fill> </v:background><![endif]--><table
                 class="es-wrapper" width="100%" cellspacing="0" cellpadding="0"><tr><td
                         valign="top"><table cellpadding="0" cellspacing="0"
@@ -210,12 +208,12 @@ if($method == "POST"){
                                                                             </h1><br><h1
                                                                             style="color:
                                                                             #ffffff;font-size:
-                                                                            28px"><b>Nombre: '.$comprador_nombre.'   </b>
+                                                                            28px"><b>Nombre: '.preg_replace("/[^A-Za-z0-9 ]/", '',iconv('UTF-8', 'ASCII//TRANSLIT', $comprador_nombre)).'   </b>
                                                                             </h1><br>
                                                                             <h1
                                                                             style="color:
                                                                             #ffffff;font-size:
-                                                                            28px"><b>Cédula: '.$comprador_cedula.' </b>
+                                                                            28px"><b>Cedula: '.$comprador_cedula.' </b>
                                                                             </h1></td></tr></table></td></tr></table></td>
                                         </tr><tr><td class="es-p40r es-p40l"
 
@@ -315,7 +313,7 @@ if($method == "POST"){
                                                                             cupo
                                                                             en
                                                                             ganatucarro.com,
-                                                                            podrás
+                                                                            podras
                                                                             visualizarla
                                                                             con
                                                                             el
@@ -406,22 +404,7 @@ if($method == "POST"){
                                                                                 width="19"
                                                                                 height="19"
                                                                                 layout="responsive"></img></a></td><td><label style="color: #ffffff;font-size:14px">
-                                                                                Miercoles,
-                                                                                24
-                                                                                de
-                                                                                diciembre
-                                                                                de
-                                                                                2021
-                                                                                a
-                                                                                las
-                                                                                0:00
-                                                                                (hora
-                                                                                estándar
-                                                                                de
-                                                                                Colombia
-                                                                                hora
-                                                                                de
-                                                                                Colombia)
+                                                                                Revisar la página oficial de gana tu carro en instagram para actualizar la fecha del sorteo.
                                                                             </label></td></tr></table></td></tr></table></td></tr></table></td>
                             </tr></table><table cellpadding="0" cellspacing="0"
                             class="es-footer" align="center"><tr><td
